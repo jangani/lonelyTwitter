99b59a47d6f2c1b4da153a8d904aed786ebc15c0
jangani <jangani@ualberta.ca>
2017-02-03 15:32:16 -0700

    Update TweetTooLongException.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
@@ -1,1 +1,8 @@
+/*
+ * Copyright (c) 2017. TEAM A, CMPUT 301 University of Alberta - All Rights Reserved. You may use, distribute or modify the code under the terms and conditions of Code of Student behavior at University of Alberta
+ * You can find a copy of the license in this project. Otherwise please contact at jangani@ualberta.ca
+ */
+package ca.ualberta.cs.lonelytwitter;
 
+public class TweetTooLongException extends Exception {
+}
